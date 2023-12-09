861b81357b7b79c8bf1347cb02ed4a12d7253854
Al-27 <ahmedlaghfour@gmail.com>
2023-12-09 19:26:17 +0000

    fix

--- console.py
+++ console.py
@@ -76,7 +76,7 @@ def check_invalid(str,id=False, attr=False):
             
             dummy = GetClass(str[0])()
             
-            if attribute == "" or not hasattr(dummy,attribute):
+            if attribute == "" : #or not hasattr(dummy,attribute):
                 raise TypeError()
         except Exception as e:
             print("** attribute name missing **")
